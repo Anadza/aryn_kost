4f19338325c7326715ff939f3576e26698cf130b
rahmals238 <rahmals238@gmail.com>
2026-07-06 22:41:15 +0700

    Menambahkan fitur data penghuni

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,36 +4,67 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=poppins:400,500,600,700&display=swap" rel="stylesheet" />
+    <!-- Google Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 
-    <!-- Scripts -->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
+        rel="stylesheet">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+
+    <!-- CSS -->
+    <style>
+        body {
+            font-family: 'Poppins', sans-serif;
+            background: #F8EFD8;
+        }
+
+        table tbody tr {
+            transition: .25s;
+        }
+
+        table tbody tr:hover {
+            transform: scale(1.005);
+        }
+    </style>
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="font-sans antialiased">
-    <div class="bg-secondary dark:bg-gray-900 min-h-screen">
+
+    <div class="min-h-screen bg-gray-100">
+
         @include('layouts.navigation')
 
-        <!-- Page Heading -->
         @isset($header)
-            <header class="bg-white dark:bg-gray-800 shadow">
-                <div class="mx-auto px-4 sm:px-6 lg:px-8 py-6 max-w-7xl">
+
+            <header class="bg-white shadow">
+
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+
                     {{ $header }}
+
                 </div>
+
             </header>
+
         @endisset
 
-        <!-- Page Content -->
         <main>
+
             {{ $slot }}
+
         </main>
+
     </div>
+
 </body>
 
 </html>
