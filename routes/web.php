e6bf72850f23f05307ed11ca73d6ab11713ddb69
Anadza <anadzazizah162@gmail.com>
2026-07-10 03:54:30 +0700

    Profile

--- routes/web.php
+++ routes/web.php
@@ -55,6 +55,10 @@ Route::middleware(['auth', 'role:owner'])
     ->name('owner.')
     ->group(function () {
 
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
         Route::get('/dashboard', [OwnerController::class, 'index'])
             ->name('dashboard');
 
