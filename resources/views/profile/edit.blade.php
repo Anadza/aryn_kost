48d6eaba2b608d00efae0cfafe61d4e90e3df675
rahmals238 <rahmals238@gmail.com>
2026-07-11 19:47:34 +0700

    Add penghuni profile interface

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -6,17 +6,27 @@
     </x-slot>
 
     <div class="py-12">
-        <div class = "max-w-7xl mx-auto sm:px-6 lg:px-8 flex flex-col md:flex-row gap-6">
-            <div class = "h-[calc(100vh-180px)] w-full md:w-1/3 bg-white dark:bg-gray-800 shadow-sm rounded-2xl border border-gray-200 dark:border-gray-700 p-4 sm:p-8">
-                <div class="max-w-xl">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex flex-col md:flex-row gap-6">
+
+            {{-- Card Kiri --}}
+            <div class="h-[calc(100vh-180px)] w-full md:w-1/3 bg-white dark:bg-gray-800 shadow-sm rounded-2xl border border-gray-200 dark:border-gray-700 p-4 sm:p-8">
+
+                <div class="w-full">
                     @include('profile.partials.profile-information')
                 </div>
+
             </div>
-            <div class = "h-[calc(100vh-180px)] w-full md:w-2/3 bg-white dark:bg-gray-800 shadow-sm rounded-2xl border border-gray-200 dark:border-gray-700 p-4 sm:p-8">
-                <div class="max-w-xl">
+
+            {{-- Card Kanan --}}
+            <div class="h-[calc(100vh-180px)] w-full md:w-2/3 bg-white dark:bg-gray-800 shadow-sm rounded-2xl border border-gray-200 dark:border-gray-700 p-4 sm:p-8">
+
+                <div class="w-full">
                     @include('profile.partials.update-profile-information-form')
                 </div>
+
             </div>
+
         </div>
     </div>
+
 </x-app-layout>
