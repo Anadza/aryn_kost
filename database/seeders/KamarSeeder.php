6db3ed6508f185e60e831f14843e7ec3df60055a
rahmals238 <rahmals238@gmail.com>
2026-07-13 22:49:33 +0700

    Edit kamar seeder

--- database/seeders/KamarSeeder.php
+++ database/seeders/KamarSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use App\Models\Kamar;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 class KamarSeeder extends Seeder
 {
@@ -20,9 +19,8 @@ class KamarSeeder extends Seeder
 
             $noKamar = 'A' . str_pad($i, 3, '0', STR_PAD_LEFT);
 
-            // Atur status kamar
             $status = match ($i) {
-                3,4,8 => 'kosong',
+                3, 4, 8 => 'kosong',
                 7 => 'booking',
                 default => 'terisi',
             };
@@ -52,7 +50,7 @@ class KamarSeeder extends Seeder
             $noKamar = 'B' . str_pad($i, 3, '0', STR_PAD_LEFT);
 
             $status = match ($i) {
-                2,5 => 'kosong',
+                2, 5 => 'kosong',
                 6 => 'booking',
                 default => 'terisi',
             };
@@ -69,36 +67,6 @@ class KamarSeeder extends Seeder
                     'status' => $status,
                 ]
             );
-        // Bersihkan data lama
-        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-        Kamar::truncate();
-        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
-
-        $data = [
-            ['no_kamar' => 'A001', 'tipe' => 'Standar', 'harga' => 1000000, 'status' => 'terisi'],
-            ['no_kamar' => 'A002', 'tipe' => 'Standar', 'harga' => 1000000, 'status' => 'terisi'],
-            ['no_kamar' => 'A003', 'tipe' => 'Standar', 'harga' => 1000000, 'status' => 'terisi'],
-            ['no_kamar' => 'A004', 'tipe' => 'Standar', 'harga' => 1000000, 'status' => 'terisi'],
-            ['no_kamar' => 'A005', 'tipe' => 'Standar', 'harga' => 1000000, 'status' => 'terisi'],
-            ['no_kamar' => 'A006', 'tipe' => 'Standar', 'harga' => 1000000, 'status' => 'terisi'],
-            ['no_kamar' => 'A007', 'tipe' => 'Standar', 'harga' => 1000000, 'status' => 'terisi'],
-            ['no_kamar' => 'A008', 'tipe' => 'Standar', 'harga' => 1000000, 'status' => 'terisi'],
-            ['no_kamar' => 'A009', 'tipe' => 'Standar', 'harga' => 1000000, 'status' => 'terisi'],
-            ['no_kamar' => 'A010', 'tipe' => 'Standar', 'harga' => 1000000, 'status' => 'terisi'],
-            ['no_kamar' => 'B001', 'tipe' => 'Deluxe', 'harga' => 1500000, 'status' => 'terisi'],
-            ['no_kamar' => 'B002', 'tipe' => 'Deluxe', 'harga' => 1500000, 'status' => 'terisi'],
-            ['no_kamar' => 'B003', 'tipe' => 'Deluxe', 'harga' => 1500000, 'status' => 'terisi'],
-            ['no_kamar' => 'B004', 'tipe' => 'Deluxe', 'harga' => 1500000, 'status' => 'terisi'],
-            ['no_kamar' => 'B005', 'tipe' => 'Deluxe', 'harga' => 1500000, 'status' => 'terisi'],
-            ['no_kamar' => 'B006', 'tipe' => 'Deluxe', 'harga' => 1500000, 'status' => 'terisi'],
-            ['no_kamar' => 'B007', 'tipe' => 'Deluxe', 'harga' => 1500000, 'status' => 'terisi'],
-            ['no_kamar' => 'B008', 'tipe' => 'Deluxe', 'harga' => 1500000, 'status' => 'terisi'],
-            ['no_kamar' => 'B009', 'tipe' => 'Deluxe', 'harga' => 1500000, 'status' => 'terisi'],
-            ['no_kamar' => 'B010', 'tipe' => 'Deluxe', 'harga' => 1500000, 'status' => 'terisi'],
-        ];
-
-        foreach ($data as $row) {
-            Kamar::create($row);
         }
     }
 }
