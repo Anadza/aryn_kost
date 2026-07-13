57621339c8e6f2dafb85947ea8097fa31999ca29
Anadza <anadzazizah162@gmail.com>
2026-07-13 08:59:50 +0700

    Perbaiki Tabel Tagihan dan Menghapus Tabel add_user_id_to_penghunis

--- database/migrations/2026_07_09_160256_create_tagihans_table.php
+++ database/migrations/2026_07_09_160256_create_tagihans_table.php
@@ -13,13 +13,24 @@ return new class extends Migration
 
             $table->unsignedBigInteger('user_id')->nullable();
 
+            $table->string('nomor_tagihan')->unique();
+            $table->integer('jumlah_tagihan');
+            $table->string('bulan_tagihan');
+            $table->string('status_pembayaran')->default('Belum Dibayar');
+            $table->string('bukti_pembayaran_path')->nullable();
+            $table->string('nama_penghuni')->nullable(); // Kolom dari file alter sebelumnya
+            $table->date('tanggal_jatuh_tempo')->nullable();
+            $table->timestamp('tanggal_upload_bukti')->nullable();
+
+            $table->timestamps(); 
+
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
     public function down(): void
     {
-        Schema::dropIfExists('tagihans');{
+        Schema::dropIfExists('tagihans'); {
         };
     }
 };
