bfdb58c677f9019aa14d87fdf87e561fbb0726e4
Kinaraa7 <nashwaaufawiguna@gmail.com>
2026-07-15 12:50:05 +0700

    perbaiki tampilan owner

--- flutter/lib/config/app_config.dart
+++ flutter/lib/config/app_config.dart
@@ -1,5 +1,5 @@
 class AppConfig {
   // Menggunakan IP lokal agar bisa diakses dari HP (Hotspot)
   // Karena Anda menjalankan php artisan serve, pastikan menggunakan port 8000
-  static const String baseUrl = 'http://172.17.128.30:8000/api';
+  static const String baseUrl = 'http://10.111.151.181:8000/api';
 }
